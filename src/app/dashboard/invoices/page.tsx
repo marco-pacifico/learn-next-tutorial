92b4732c4b542d0fee679989834d1c946c4e815b
Marco Pacifico <marco.pacifico@gmail.com>
2024-01-18 15:11:07 -0300

    filter table based on search params

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,35 +1,47 @@
 import Link from "next/link";
-import { sql } from "@vercel/postgres";
 import { DeleteInvoice } from "@/app/ui/invoices/forms";
 import Search from "@/app/ui/search";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: 'Invoices',
+  title: "Invoices",
 };
 
+export default async function InvoicesPage({
+  searchParams,
+}: {
+  searchParams: {
+    search?: string;
+    page?: string;
+  };
+}) {
+  const search = searchParams.search || "";
+  const currentPage = searchParams.page ? parseInt(searchParams.page) : 1;
+  const invoices = await fetchFilteredInvoices(search, currentPage);
 
-export default async function InvoicesPage() {
-    const invoices = (await sql`SELECT * FROM invoices`).rows;
-    console.log({ invoices });
-    return (
-        <>
-            <h1>Invoice</h1>
-            <p>Invoice page</p>
-            <div className="flex gap-8 justify-between items-center">
-            <Search />
-            <Link href="/dashboard/invoices/create">Create invoice</Link>
+  return (
+    <>
+      <h1 className="text-3xl my-4">Invoices</h1>
+
+      <div className="flex gap-8 justify-between items-center mb-4">
+        <Search />
+        <Link href="/dashboard/invoices/create" className="px-4 py-2">Create invoice</Link>
+      </div>
+
+      <ul>
+        {invoices.map((invoice) => (
+          <li key={invoice.id} className="flex justify-between border-b py-2">
+            <div className="flex gap-6 w-full">
+              <p className="flex-1">{invoice.name}</p>
+              <p className="w-20">{invoice.status}</p>
+              <p className="w-10 text-right mr-4">${invoice.amount / 100}</p>
+              <Link href={`/dashboard/invoices/${invoice.id}/edit`}>Edit</Link>
+              <DeleteInvoice id={invoice.id} />
             </div>
-            <ul>
-                {invoices.map((invoice) => (
-                    <li key={invoice.id} className="flex justify-between border-b py-2">
-                        <Link href={`/dashboard/invoices/${invoice.id}/edit`}>
-                            ${invoice.amount / 100}
-                        </Link>
-                        <DeleteInvoice id={invoice.id} />
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
