c7437db06bc770f386e4d153cfd329cb11f2aa38
Marco Pacifico <marco.pacifico@gmail.com>
2024-01-18 07:40:45 -0300

    move cancel button

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,10 +4,10 @@ import SignUpForm from "@/app/ui/signup/sign-up-form";
 export default function SignUpPage() {
     return (
         <>
+            <Link href="/" className="px-4 py-2 hover:bg-gray-900 rounded-md mt-2 block w-full text-center">Cancel</Link>
             <h1>Sign Up</h1>
             <p>Sign up to access your dashboard</p>
             <SignUpForm />
-            <Link href="/" className="px-4 py-2 hover:bg-gray-900 rounded-md mt-2 block w-full text-center">Cancel</Link>
         </>
     )
 }
