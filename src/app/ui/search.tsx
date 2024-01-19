15ebac653d5fde4628834a11cbe5dbcd85cfb7c0
Marco Pacifico <marco.pacifico@gmail.com>
2024-01-18 23:09:30 -0300

    Add useTransition and useState hooks to Search component

--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useTransition } from "react";
+import { useTransition, useState } from "react";
 
 export default function Search() {
   const searchParams = useSearchParams();
@@ -9,19 +9,26 @@ export default function Search() {
   const { replace } = useRouter();
 
   const [pending, startTransition] = useTransition();
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
+  // const isSearching = timeoutId || pending;
 
   function handleChange(value: string) {
-    startTransition(() => {
-      const params = new URLSearchParams(searchParams);
-      if (value) {
-        params.set("search", value);
-        params.set("page", "1");
-      } else {
-        params.delete("search");
-        params.delete("page");
-      }
-      replace(`${pathname}?${params.toString()}`);
-    });
+    clearTimeout(timeoutId);
+    let id = setTimeout(() => {
+      startTransition(() => {
+        const params = new URLSearchParams(searchParams);
+        if (value) {
+          params.set("search", value);
+          params.set("page", "1");
+        } else {
+          params.delete("search");
+          params.delete("page");
+        }
+        replace(`${pathname}?${params.toString()}`);
+        // setTimeoutId(undefined);
+      });
+    }, 500);
+    setTimeoutId(id);
   }
 
   return (
@@ -33,10 +40,12 @@ export default function Search() {
           className="text-gray-900 w-full disabled:opacity-40"
           onChange={(e) => handleChange(e.target.value)}
           defaultValue={searchParams.get("search")?.toString()}
-          disabled={pending}
+          // disabled={pending}
         />
       </label>
-      {pending && <div className="absolute top-1/4 right-0 pr-12">Searching...</div>}
+      {pending && (
+        <div className="absolute top-1/4 right-0 pr-12"><span className="text-gray-900">Searching...</span></div>
+      )}
     </div>
   );
 }
