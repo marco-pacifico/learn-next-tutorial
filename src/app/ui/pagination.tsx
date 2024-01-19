9689286406f7050de1d904258e7b4939d03971b1
Marco Pacifico <marco.pacifico@gmail.com>
2024-01-18 22:41:53 -0300

    Add useTransition hook to Pagination and Search components

--- src/app/ui/pagination.tsx
+++ src/app/ui/pagination.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
@@ -7,10 +8,14 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const { replace } = useRouter();
   const currentPage = Number(searchParams.get("page")) || 1;
 
+  const [pending, startTransition] = useTransition();
+
   function updateUrl(page: number | string) {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", page.toString());
-    replace(`${pathname}?${params.toString()}`);
+    startTransition(() => {
+      const params = new URLSearchParams(searchParams);
+      params.set("page", page.toString());
+      replace(`${pathname}?${params.toString()}`);
+    });
   }
 
   // If there are no invoices, don't show pagination
@@ -18,25 +23,31 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   return (
     <div className="flex my-4 mr-4">
-      <p className="flex-1">{`Page ${currentPage} of ${totalPages}`}</p>
+      <p className="flex-1">
+        {pending ? "Loading..." : `Page ${currentPage} of ${totalPages}`}
+      </p>
       <div className="flex gap-4">
         {/* Previous Button: Only show if past page 1 */}
         {currentPage > 1 && (
-          <button onClick={() => updateUrl(currentPage - 1)}>Previous</button>
+          <button
+            onClick={() => updateUrl(currentPage - 1)}
+            disabled={pending}
+            className="disabled:opacity-40 disabled:cursor-not-allowed"
+          >
+            Previous
+          </button>
         )}
         {/* Next Button: Only show if there's more than one page */}
         {totalPages > 1 && (
           <button
             onClick={() => updateUrl(currentPage + 1)}
-            disabled={currentPage === totalPages}
+            disabled={currentPage === totalPages || pending}
             className="disabled:opacity-40 disabled:cursor-not-allowed"
           >
             Next
           </button>
-        
         )}
       </div>
     </div>
   );
 }
-
--- src/app/ui/search.tsx
+++ src/app/ui/search.tsx
@@ -1,33 +1,42 @@
 "use client";
 
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 export default function Search() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
+  const [pending, startTransition] = useTransition();
+
   function handleChange(value: string) {
-    const params = new URLSearchParams(searchParams);
-    if (value) {
-      params.set("search", value);
-      params.set("page", "1");
-    } else {
-      params.delete("search");
-      params.delete("page");
-    }
-    replace(`${pathname}?${params.toString()}`);
+    startTransition(() => {
+      const params = new URLSearchParams(searchParams);
+      if (value) {
+        params.set("search", value);
+        params.set("page", "1");
+      } else {
+        params.delete("search");
+        params.delete("page");
+      }
+      replace(`${pathname}?${params.toString()}`);
+    });
   }
 
   return (
-    <label className="flex-1">
-      <input
-        type="search"
-        placeholder="Search"
-        className="text-gray-900 w-full"
-        onChange={(e) => handleChange(e.target.value)}
-        defaultValue={searchParams.get("search")?.toString()}
-      />
-    </label>
+    <div className="flex-1 relative">
+      <label>
+        <input
+          type="search"
+          placeholder="Search"
+          className="text-gray-900 w-full disabled:opacity-40"
+          onChange={(e) => handleChange(e.target.value)}
+          defaultValue={searchParams.get("search")?.toString()}
+          disabled={pending}
+        />
+      </label>
+      {pending && <div className="absolute top-1/4 right-0 pr-12">Searching...</div>}
+    </div>
   );
 }
